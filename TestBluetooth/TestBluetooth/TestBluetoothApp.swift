38d2003d44e001b68f119f7bd3acba8676202f4f
SarahSilvaSales <sarasilvasales@gmail.com>
2023-06-15 21:22:31 -0400

    criacao do projeto TestBluetooth

---
+++ TestBluetooth/TestBluetooth/TestBluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBluetoothApp.swift
+//  TestBluetooth
+//
+//  Created by projeto smart on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TestBluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
