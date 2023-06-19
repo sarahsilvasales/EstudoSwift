eeca9f6e95dfbdcfd1c5d077315af6f052303f76
SarahSilvaSales <sarasilvasales@gmail.com>
2023-06-19 15:50:01 -0400

    criacao do projeto PhotoPicker

---
+++ PhotoPicker/PhotoPicker/PhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerApp.swift
+//  PhotoPicker
+//
+//  Created by projeto smart on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
